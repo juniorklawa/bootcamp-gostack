e976b38aff6eefe823dd6657d08c5c6d97216f07
Usuario <usuario@MacBook-Pro.local>
2020-01-21 22:55:10 -0300

    wip

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World!' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Everaldo',
+    email: 'everaldo@gmail.com',
+    password_hash: '12345',
+  });
+  return res.json(user);
 });
 
 export default routes;
