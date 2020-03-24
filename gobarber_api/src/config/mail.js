1d89ad1a27593b31ce85d912df988dee67913e79
ejwindows <juniorklawa10@gmail.com>
2020-03-24 10:39:45 -0300

    configurando Nodemailer

---
+++ gobarber_api/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'de870b6091d566',
+    pass: '85f549feb7b306',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
