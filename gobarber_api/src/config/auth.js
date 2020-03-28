5881914000e3b8d4d79ff6b79558da6f210570d6
ejwindows <juniorklawa10@gmail.com>
2020-03-28 16:25:44 -0300

    variaveis de ambiente

--- gobarber_api/src/config/auth.js
+++ gobarber_api/src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '0ed8e053d48eb628e6c2167a2fef0505',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- gobarber_api/src/config/sentry.js
+++ gobarber_api/src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://003547ed880b497f9800dd3fb6fcbf14@sentry.io/5178573',
+  dsn: process.env.SENTRY_DSN,
 };
