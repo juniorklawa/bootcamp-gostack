52a28b35a29bd3f5f2efb2e4376696482c9fc523
ejwindows <juniorklawa10@gmail.com>
2020-04-15 18:28:15 -0300

    added mobile

---
+++ nivel01/mobile/src/index.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  Text,
+  FlatList,
+  StyleSheet,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
+import api from "./services/api";
+
+export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    try {
+      api.get("projects").then((response) => {
+        setProjects(response.data);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post("projects", {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Everaldo Junior",
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+        <TouchableOpacity
+          onPress={handleAddProject}
+          activeOpacity={0.6}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159c1",
+  },
+
+  project: {
+    color: "#fff",
+    fontSize: 20,
+  },
+
+  button: {
+    alignSelf: "stretch",
+    backgroundColor: "#FFF",
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
