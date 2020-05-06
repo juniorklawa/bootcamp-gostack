adab666961e097c2b77cbaa94edb166a9b2c9824
EveraldoJr@BB-8 <juniorklawa10@gmail.com>
2020-05-06 18:22:14 -0300

    finished project

--- nivel03/05-primeiro-projeto-react/src/routes/index.tsx
+++ nivel03/05-primeiro-projeto-react/src/routes/index.tsx
@@ -5,9 +5,9 @@ import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Dashboard}/>
-    <Route path="/repository" component={Repository}/>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repositories/:repository+" component={Repository} />
   </Switch>
-)
+);
 
 export default Routes;
