3809f0a59a0e0d5ea6236d75468f03f1f718ac5e
everaldo@bb-8 <juniorklawa10@gmail.com>
2020-06-23 19:16:56 -0300

    Liskov principle

--- nivel02/primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentService.ts
+++ nivel02/primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentService.ts
@@ -2,17 +2,17 @@ import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
 
 import AppError from '../../../shared/errors/AppError';
 
-interface Request {
+interface IRequest {
   provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider_id }: Request): Promise<Appointment> {
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
     const appointmentDate = startOfHour(date);
 
