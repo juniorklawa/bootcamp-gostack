de0369d4e435c9d3330183bc99ce2bd48f46cd86
everaldo@bb-8 <juniorklawa10@gmail.com>
2020-07-02 19:53:17 -0300

    tests

--- nivel02/primeiro-projeto-node/src/shared/container/providers/index.ts
+++ nivel02/primeiro-projeto-node/src/shared/container/providers/index.ts
@@ -1,6 +1,9 @@
 import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+// import IMailProvider from './MailProvider/models/IMailProvider';
+
+// container.registerSingleton<IMailProvider>('MailProvider', DiskStorageProvider);
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
